92e34d206c1dbb2c031e7f87164d0261919055f3
divgray <divgray20@gmail.com>
2025-03-22 20:58:29 +0800

    COMMIT

--- temples.js
+++ temples.js
@@ -4,7 +4,7 @@ const hamMenu = document.getElementById(".ham-menu");
 const offScreenMenu = document.getElementById(".off-screen-menu");
 
 
-hamMenu.addEventListener("click", () => {
+hamMenu.getElementById("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
 });
